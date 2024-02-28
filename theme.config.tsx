9ed1ee7495e4de4690d77f09887fc8bfe7100709
KoriIku <86508666+KoriIku@users.noreply.github.com>
2024-02-28 17:55:17 +0800

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -9,7 +9,7 @@ const config: DocsThemeConfig = {
   chat: {
     link: 'https://t.me/+PindoE9yvIpmOWI9',
   },
-  docsRepositoryBase: 'https://github.com/KoriIku/jhentai',
+  docsRepositoryBase: 'https://github.com/KoriIku/jhentai/tree/main',
   footer: {
     text: '可能是最好用的跨平台e-hentai客户端',
   },
